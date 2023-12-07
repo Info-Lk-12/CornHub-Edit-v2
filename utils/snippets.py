14eddbf2cf012316c0adc3e16fd54207f8c12a82
david <davissoftware6@gmail.com>
2023-12-07 11:49:31 +0100

    bug fix

--- utils/snippets.py
+++ utils/snippets.py
@@ -1,3 +1,3 @@
 SNIPPETS = {
-    "email": "Sehr geehrter Herr/Frau, \n\n [Text hier einfügen] \n\n Mit freundlichen Grüßen"
+    "email": "Sehr geehrter Herr/Frau,\n\n[Text hier einfügen]\n\nMit freundlichen Grüßen"
 }
